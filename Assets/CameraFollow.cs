e639b6c38cd923e23a266bccfe99c9d79ade89d5
Ben <ben.lawrence0324@gmail.com>
2023-04-15 00:42:03 -0500

    yeah...

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraDistance = 10.0f;
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position - player.transform.forward * cameraDistance;
+        transform.LookAt(player.transform.position);
+        transform.position = new Vector3(transform.position.x, transform.position.y + 5, transform.position.z);
+    }
+}
