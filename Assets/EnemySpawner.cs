82315901b8f1b566e9fbc4f338a750897013e150
Ben <ben.lawrence0324@gmail.com>
2023-04-16 03:30:57 -0500

    Enemy Spawner Maybe

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SuperPupSystems.Helper;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float radiusToSpawn = 80;
+    public List<GameObject> enemies = new List<GameObject>();
+    public float minTime = 10f;
+    public float maxTime = 30;
+
+    public GameObject particles;
+
+    private GameObject playerShip;
+    private Timer timer;
+
+    private void Start()
     {
-        
+        playerShip = FindObjectOfType<NewShipController>().gameObject;
+        timer = GetComponent<Timer>();
+        timer.TimeOut.AddListener(RestartTimer);
+        timer.TimeOut.AddListener(SpawnEnemy);
+        timer.StartTimer(RandomTime(), false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnEnemy()
     {
-        
+        foreach(GameObject enemy in enemies)
+        {
+            Vector3 randomPos = (Random.insideUnitSphere * radiusToSpawn) + playerShip.transform.position;
+
+            Instantiate(particles, randomPos, Quaternion.identity);
+
+            Instantiate(enemy, randomPos, Quaternion.identity);
+        }
+    }    
+
+    private float RandomTime()
+    {
+        return Random.Range(minTime, maxTime);
+    }
+
+    private void RestartTimer()
+    {
+        timer.StartTimer(RandomTime(), false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(FindObjectOfType<NewShipController>().transform.position, radiusToSpawn);
     }
 }
