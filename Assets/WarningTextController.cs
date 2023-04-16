1b1ed4cfda29b6452d3f4303381779deeedee69c
Ben <ben.lawrence0324@gmail.com>
2023-04-16 14:12:41 -0500

    more ui and fixes

--- Assets/WarningTextController.cs
+++ Assets/WarningTextController.cs
@@ -16,23 +16,33 @@ public class WarningTextController : MonoBehaviour
 
     [TextArea]
     public string secondWarningMessage;
+    private Color originalColor;
 
     private void Start()
     {
         warningText.SetText(firstWarningMessage);
+        originalColor = warningText.color;
         FindObjectOfType<Transmitter>().finalDistanceWarning.AddListener(ChangeText);
 
         warningElement.LeanAlpha(lowAlphaValue, fadeTime).setOnComplete(FadeToFull);
     }
 
+
+    void updateValueExampleCallback(Color val)
+    {
+        warningText.color = val;
+    }
+
     public void FadeToFull()
     {
-        warningElement.LeanAlpha(1f, fadeTime).setOnComplete(FadeToEmpty);
+        LeanTween.value(gameObject, updateValueExampleCallback, Color.red, originalColor, 1f).setOnComplete(FadeToEmpty);//);
+        warningElement.LeanAlpha(1f, fadeTime);//.setOnComplete(FadeToEmpty);
     }
 
     public void FadeToEmpty()
     {
-        warningElement.LeanAlpha(lowAlphaValue, fadeTime).setOnComplete(FadeToFull);
+        LeanTween.value(gameObject, updateValueExampleCallback, originalColor, Color.red, 1f).setOnComplete(FadeToFull);//.setEase(LeanTweenType.easeOutElastic);
+        warningElement.LeanAlpha(lowAlphaValue, fadeTime);
     }
 
     public void ChangeText()
