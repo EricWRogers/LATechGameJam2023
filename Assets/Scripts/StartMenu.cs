2f76c1efb9ea251c1dc15dcd2a5b828754a44a98
Melisa Rogers <maj4683@gmail.com>
2023-04-16 11:19:48 -0500

    Hopefully fixes font and add cursor visible in Start Menu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class StartMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
     public void Quit()
     {
         Application.Quit();
