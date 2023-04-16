2cfa37a8a0297cc2f1b2d7671edbc2a74402c943
EricWRogers <ericwilliamrogers@gmail.com>
2023-04-15 20:58:49 -0500

    Transmitter logic

---
+++ Assets/Scripts/Transmitter.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Transmitter : MonoBehaviour
+{
+    public UnityEvent distanceWarning;
+    public UnityEvent finalDistanceWarning;
+    public UnityEvent lostConnection;
+    public UnityEvent returnedToSafeRange;
+
+    public bool distanceWarningActive = false;
+    public bool finalDistanceWarningActive = false;
+    public bool lostConnectionActive = false;
+
+    public float safeDistanceArea = 250.0f;
+    public float finalWarningDistance = 275.0f;
+    public float lostConnectionDistance = 300.0f;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = FindObjectOfType<NewShipController>().gameObject;
+    }
+
+    public void CheckConnection()
+    {
+        float distance = Vector3.Distance(player.gameObject.transform.position, transform.position);
+        Debug.Log(distance);
+        if (safeDistanceArea > distance)
+        {
+            if (distanceWarningActive || finalDistanceWarningActive)
+                returnedToSafeRange.Invoke();
+            
+            distanceWarningActive = false;
+            finalDistanceWarningActive = false;
+            return;
+        }
+
+        if (safeDistanceArea < distance && !distanceWarningActive)
+        {
+            distanceWarningActive = true;
+            distanceWarning.Invoke();
+        }
+
+        if (finalWarningDistance < distance && !finalDistanceWarningActive)
+        {
+            finalDistanceWarningActive = true;
+            finalDistanceWarning.Invoke();
+        }
+
+        if (lostConnectionDistance < distance && !lostConnectionActive)
+        {
+            lostConnectionActive = true;
+            lostConnection.Invoke();
+        }
+    }
+}
